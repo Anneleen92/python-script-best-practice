f1d1340fe1e33b2971a5c20fb1ace9d4ad8c48a7
Anneleen Swanepoel <anneleen.swanepoel@gmail.com>
2026-03-02 15:40:26 -0600

    Added tests to docstrings

--- smallest_factor.py
+++ smallest_factor.py
@@ -1,11 +1,35 @@
 #! /usr/bin/env python3
 
-# A script for getting the smallest prime factor of an integer.
+"A module for getting the smallest prime factor of an integer."
 
 import sys
 
 
 def get_smallest_prime_factor(n):
+    """
+    Returns the smallest integer that is a factor of `n`.
+
+    If `n` is a prime number `None` is returned.
+
+    Parameters
+    __________
+    n : int
+        The integer to be factored.
+
+    Returns
+    _______
+    int or None
+        The smallest integer that is a factor of `n`
+        or None if `n` is a prime.
+
+    Examples
+    ________
+    >>> get_smallest_prime_factor(7)
+    >>> get_smallest_prime_factor(8)
+    2
+    >>> get_smallest_prime_factor(9)
+    3
+    """
     for i in range(2, n):
         if (n % i) == 0:
             return i
